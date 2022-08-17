04138c5cbcdebe8e660f6888229574b74bd9ba20
Hrishabh Palsra <66557570+hr1shabh@users.noreply.github.com>
2022-08-18 00:01:41 +0530

    Update Heap.cpp

--- Heap/Heap.cpp
+++ Heap/Heap.cpp
@@ -21,8 +21,14 @@ int left = 2*idx;
 int right = left+1;
 int last = v.size()-1;
 int min = idx;
-if(left<=last and v[min]>v[left]) min = left; //for minheap , In case of maxheap v[min]<v[left]
-if(right<=last and v[min]>v[right]) min = right;
+if(minheap){
+    if(left<=last and v[min]>v[left]) min = left; //for minheap , In case of maxheap v[min]<v[left]
+    if(right<=last and v[min]>v[right]) min = right;
+}
+else{
+    if(left<=last and v[min]<v[left]) min = left; 
+    if(right<=last and v[min]<v[right]) min = right;
+}
 if(min != idx){
     swap(v[min],v[idx]);
     heapify(min);
@@ -36,22 +42,22 @@ heap(bool minheap){
 }
 
 void push(int data){
-v.push_back(data);
-int i = v.size()-1;
-int parent = i/2;
-if(minheap){
-while(i>1 and v[parent] > v[i]){
-    swap(v[parent],v[i]);
-    i = parent;
-    parent/=2;
-}}
-else{
-while(i>1 and v[parent] > v[i]){
-    swap(v[parent],v[i]);
-    i = parent;
-    parent/=2;
-}
-}
+    v.push_back(data);
+    int i = v.size()-1;
+    int parent = i/2;
+    if(minheap){
+    while(i>1 and v[parent] > v[i]){
+        swap(v[parent],v[i]);
+        i = parent;
+        parent/=2;
+    }}
+    else{
+    while(i>1 and v[parent] < v[i]){
+        swap(v[parent],v[i]);
+        i = parent;
+        parent/=2;
+    }
+    }
 }
 
 int top(){
@@ -59,9 +65,9 @@ int top(){
 }
 
 void pop(){
-swap(v[1],v[v.size()-1]);
-v.pop_back();
-heapify(1);
+    swap(v[1],v[v.size()-1]);
+    v.pop_back();
+    heapify(1);
 }
 
 bool empty(){
@@ -71,18 +77,18 @@ bool empty(){
 };
 
 int main(){
-int n;
-cin>>n;
-heap h(1);
-while(n--){
-    int x;
-    cin>>x;
-    h.push(x);
-   // cout<<h.top()<<" ";
-}
-while(!h.empty()){
-    cout<<h.top()<<" ";
-    h.pop();
-}
+    int n;
+    cin>>n;
+    heap h(0);
+    while(n--){
+        int x;
+        cin>>x;
+        h.push(x);
+        // cout << h.top() << " ";
+    }
+    while(!h.empty()){
+        cout<<h.top()<<" ";
+        h.pop();
+    }
 
 }
